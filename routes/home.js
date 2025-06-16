60932beaef5efdc94160d0308ad72678d9c2ed68
Blade34242 <153329304+blade34242@users.noreply.github.com>
2025-06-16 14:10:15 +0700

    Use async fs.readFile in home route

--- routes/home.js
+++ routes/home.js
@@ -3,15 +3,26 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
-router.get('/',(req,res,next) => {
+router.get('/', (req, res, next) => {
+    const pwd = path.join(__dirname, '../public/ress/mountedRess/me.json');
 
-    pwd =  path.join(__dirname,'../public/ress/mountedRess/me.json');
-    var obj = JSON.parse(fs.readFileSync(pwd, 'utf8'));
+    fs.readFile(pwd, 'utf8', (err, data) => {
+        if (err) {
+            return next(err);
+        }
 
-    const viewsData = {
-        data: obj
-    };
-    res.render('cv',viewsData);
+        let obj;
+        try {
+            obj = JSON.parse(data);
+        } catch (parseErr) {
+            return next(parseErr);
+        }
+
+        const viewsData = {
+            data: obj
+        };
+        res.render('cv', viewsData);
+    });
 });
 
 module.exports = router
